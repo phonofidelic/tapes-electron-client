04607c589be8687a9f56f17342393a2913ab5b9e
Christopher Clemons <chris.clemons85@gmail.com>
2021-04-17 15:56:02 -0700

    add initialization of Threads DB store

--- src/services/TextileProvider.tsx
+++ src/services/TextileProvider.tsx
@@ -7,11 +7,17 @@ import React, {
 } from 'react';
 import {
   Buckets,
+  Client,
+  Users,
   PushPathResult,
   KeyInfo,
   PrivateKey,
   WithKeyInfoOptions,
 } from '@textile/hub';
+
+const THREADS_DB_NAME = 'tapes-thread-db';
+
+declare const USER_API_KEY: any;
 
 interface TextileProviderProps {
   children: ReactElement;
@@ -57,8 +63,50 @@ function TextileProvider({ children }: TextileProviderProps) {
     }
   };
 
+  const initUserThread = async () => {
+    const identity = await getIdentity();
+    console.log('initUserThread, identity:', identity);
+
+    const keyInfo: KeyInfo = {
+      key: USER_API_KEY,
+    };
+
+    /**
+     * Initialize Textile User
+     */
+    const user = await Users.withKeyInfo(keyInfo);
+    await user.getToken(identity);
+
+    /**
+     * Initialize Threads client
+     */
+    const client = await Client.withKeyInfo(keyInfo);
+    await client.getToken(identity);
+
+    /**
+     * Attempt to find existing DB thread
+     */
+    let dbThread;
+    try {
+      dbThread = await user.getThread(THREADS_DB_NAME);
+      console.log('Remote Thread DB store found');
+    } catch (err) {
+      if (err.code === 5) {
+        console.log('Could not find DB Thread:', err);
+        /**
+         * Create new DB Thread
+         */
+        console.log('Creating new store on remote node...');
+        dbThread = await client.newDB(undefined, THREADS_DB_NAME);
+      } else {
+        throw err;
+      }
+    }
+    console.log('initUserThread, dbThread:', dbThread);
+  };
+
   useEffect(() => {
-    getIdentity();
+    initUserThread();
   }, []);
 
   return (
