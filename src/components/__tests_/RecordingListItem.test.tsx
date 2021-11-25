477147c1928a54374cd2edd697edf04a6291b5e4
Christopher Clemons <chris.clemons85@gmail.com>
2021-11-25 14:45:01 -0800

    fix tests

--- src/components/__tests_/RecordingListItem.test.tsx
+++ src/components/__tests_/RecordingListItem.test.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, fireEvent } from '@testing-library/react';
 import dayjs from 'dayjs';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import dayjsDuration from 'dayjs/plugin/duration';
 import { Recording } from '../../common/Recording.interface';
 import { RecordingsListItem } from '../RecordingsListItem';
 import { RecordingFormats } from '../../common/RecordingFormats.enum';
+import { RecorderState } from '../../store/types';
+import { initialState } from '../../store/reducer';
 
 dayjs.extend(customParseFormat);
 dayjs.extend(advancedFormat);
@@ -28,10 +32,12 @@ const mockRecording: Recording = {
 let mockSelect: (recording: Recording) => void;
 let mockDelete: (recordingId: string) => void;
 let mockEdit: (recordingId: string) => void;
+let mockDownload: (recordingId: string) => void;
 beforeEach(() => {
   mockSelect = jest.fn();
   mockDelete = jest.fn();
   mockEdit = jest.fn();
+  mockDownload = jest.fn();
 
   /**
    * https://github.com/jsdom/jsdom/issues/2155#issuecomment-366703395
@@ -44,32 +50,35 @@ beforeEach(() => {
   };
 });
 
-it('displays the default list item', () => {
-  const { getByText } = render(
-    <RecordingsListItem
-      bucketToken="123abc"
-      recording={mockRecording}
-      selectedRecording={null}
-      handleSelectRecording={mockSelect}
-      handleDeleteRecording={mockDelete}
-      handleEditRecording={mockEdit}
-    />
+const renderMockedComponent = (state: RecorderState) => {
+  const mockStore = createStore(() => state);
+
+  return render(
+    <Provider store={mockStore}>
+      <RecordingsListItem
+        recording={mockRecording}
+        selectedRecording={mockStore.getState().selectedRecording}
+        caching={mockStore.getState().caching}
+        handleSelectRecording={mockSelect}
+        handleDeleteRecording={mockDelete}
+        handleEditRecording={mockEdit}
+        handleDownloadRecording={mockDownload}
+      />
+    </Provider>
   );
+};
+
+it('displays the default list item', () => {
+  const { getByText } = renderMockedComponent(initialState);
 
   expect(getByText('Mock recording')).toBeInTheDocument();
 });
 
 it('displays detail info when selected', () => {
-  const { getByText } = render(
-    <RecordingsListItem
-      bucketToken="123abc"
-      recording={mockRecording}
-      selectedRecording={mockRecording}
-      handleSelectRecording={mockSelect}
-      handleDeleteRecording={mockDelete}
-      handleEditRecording={mockEdit}
-    />
-  );
+  const { getByText } = renderMockedComponent({
+    ...initialState,
+    selectedRecording: mockRecording,
+  });
 
   expect(
     getByText(
