2ae895b8ce1d0b0aff4f58f48c95d64ca0bf150f
Christopher Clemons <chris.clemons85@gmail.com>
2022-07-28 14:49:38 -0700

    chore: fix typo in filename

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Main } from './electron/Main';
 import { NewRecordingChannel } from './electron/channels/NewRecordingChannel';
 import { DeleteRecordingChannel } from './electron/channels/DeleteRecordingChannel';
 import { UploadAudioFileChannel } from './electron/channels/UploadAudioFileChannel';
-import { SetInputDeviceChannel } from './electron/channels/SetInputDevaiceChannel';
+import { SetInputDeviceChannel } from './electron/channels/SetInputDeviceChannel';
 import { CacheRecordingChannel } from './electron/channels/CacheRecordingChannel';
 import { GetRecordingStorageStatusChannel } from './electron/channels/GetRecordingStorageStatusChannel';
 import { DeployWebClientChannel } from './electron/channels/DeployWebClinetChannel'
