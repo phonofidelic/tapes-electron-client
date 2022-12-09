6a53060972ee6501dbfe8b7bd9fcbab18963cc30
Christopher Clemons <chris.clemons85@gmail.com>
2022-12-09 03:40:45 +0100

    fix: add moduleNameMapper in jest.config

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/src/__e2e__/index.test.ts', 'src/db/__tests__'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/src/__e2e__/index.test.ts',
+    'src/db/__tests__',
+  ],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   globals: {
     USER_API_KEY: process.env.USER_API_KEY,
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
