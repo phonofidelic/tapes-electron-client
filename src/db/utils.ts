a93d87af22f59ee6517c84e8606bfa9f33dad4e2
Christopher Clemons <chris.clemons85@gmail.com>
2022-12-27 00:11:38 +0100

    fix: add module declaration for WebRTCStar

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,59 +1,55 @@
 import { create } from 'ipfs-core';
-import type { IPFS } from 'ipfs-core-types'
-//@ts-ignore
+import type { IPFS } from 'ipfs-core-types';
 import WebRTCStar from 'libp2p-webrtc-star';
+// import { webRTCStar } from '@libp2p/webrtc-star';
 import { NOISE } from '@chainsafe/libp2p-noise';
 // import * as MockIPFS from 'mockipfs'
 
-declare const LIBP2P_SIG_SERVER: string
+declare const LIBP2P_SIG_SERVER: string;
 
 export async function createIpfsNode(): Promise<IPFS> {
   const config = {
     libp2p: {
       modules: {
-        transports: [
-          WebRTCStar,
-        ]
+        transports: [WebRTCStar],
       },
       config: {
         peerDiscovery: {
           webRTCStar: {
-            enabled: true
-          }
+            enabled: true,
+          },
         },
         transport: {
           WebRTCStar: {
-            connEncryption: [NOISE]
+            connEncryption: [NOISE],
           },
         },
       },
-      transportManager: { faultTolerance: 1 }
+      transportManager: { faultTolerance: 1 },
     },
     relay: {
       enabled: true,
       hop: {
         enabled: true,
-        active: true
-      }
+        active: true,
+      },
     },
     config: {
       //@ts-ignore
       Bootstrap: [],
       Addresses: {
         //@ts-ignore
-        Swarm: [
-          LIBP2P_SIG_SERVER
-        ],
-      }
+        Swarm: [LIBP2P_SIG_SERVER],
+      },
     },
     repo: process.env.NODE_ENV === 'test' ? './ipfs-test' : './ipfs',
-    EXPERIMENTAL: { pubsub: true }
-  }
-  console.log('*** sig server:', LIBP2P_SIG_SERVER)
+    EXPERIMENTAL: { pubsub: true },
+  };
+  console.log('*** sig server:', LIBP2P_SIG_SERVER);
   //@ts-ignore
   // const node = process.env.NODE_ENV === 'test' ? MockIPFS.getLocal() : await create(config)
-  const node = await create(config)
+  const node = await create(config);
 
   //@ts-ignore
-  return node
+  return node;
 }
