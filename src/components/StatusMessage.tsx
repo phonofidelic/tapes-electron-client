377bbd2011eb19f9103f7c76afe599e9c4d73f25
Christopher Clemons <chris.clemons85@gmail.com>
2023-02-19 17:51:24 +0100

    chore: fmt changes and remove unused imports

--- src/components/StatusMessage.tsx
+++ src/components/StatusMessage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
 import { RecorderState } from '../store/types';
 
@@ -9,35 +9,37 @@ import Typography from '@mui/material/Typography';
 type StatusMessageProps = {
   loading: boolean;
   loadingMessage: string | null;
-}
+};
 
-export function StatusMessage({
-  loading,
-  loadingMessage
-}: StatusMessageProps) {
-  const theme = useTheme()
+export function StatusMessage({ loading, loadingMessage }: StatusMessageProps) {
+  const theme = useTheme();
 
   return (
     <Fade in={loading}>
-      <div style={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        textAlign: 'center',
-        backgroundColor: `rgba(0,0,0, .75)`,
-        borderRadius: '8px',
-        margin: 8
-      }}>
-        <Typography style={{ color: '#fff' }} variant="caption">{loadingMessage}</Typography>
+      <div
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          backgroundColor: `rgba(0,0,0, .75)`,
+          borderRadius: '8px',
+          margin: 8,
+          zIndex: theme.zIndex.appBar,
+        }}
+      >
+        <Typography style={{ color: '#fff' }} variant="caption">
+          {loadingMessage}
+        </Typography>
       </div>
     </Fade>
-  )
+  );
 }
 
 const mapStateToProps = (state: RecorderState) => ({
   loading: state.loading,
-  loadingMessage: state.loadingMessage
-})
+  loadingMessage: state.loadingMessage,
+});
 
-export default connect(mapStateToProps, null)(StatusMessage)
+export default connect(mapStateToProps, null)(StatusMessage);
